3d7278cc755c16ab48523e3c065b3ce5f74b35f0
Lth <648120201@qq.com>
2019-06-23 16:52:58 +0800

    加入胜利后的界面

--- CCC/CCC/new.cpp
+++ CCC/CCC/new.cpp
@@ -14,17 +14,33 @@ void draw_chess_board(int chess_board[][15]);
 int one_side_win(int which_side);
 int is_five(int row,int column,int which_side);
 void win(int which_side);
+void save_game_record();
 void play();
 //棋盘范围
-#define CHESS_UP 207
-#define CHESS_DOWN 906
-#define CHESS_LEFT 98
-#define CHESS_RIGHT 772
+#define CHESS_UP 98
+#define CHESS_DOWN 772
+#define CHESS_LEFT 207
+#define CHESS_RIGHT 906
 //悔棋按钮范围
-#define WITHDRAW_UP 51
-#define WITHDRAW_DOWN 144
-#define WITHDRAW_LEFT 366
-#define WITHDRAW_RIGHT 404
+#define WITHDRAW_UP 35
+#define WITHDRAW_DOWN 73
+#define WITHDRAW_LEFT 331
+#define WITHDRAW_RIGHT 425
+//返回菜单按钮范围
+#define RETURN_UP 272
+#define RETURN_DOWN 412
+#define RETURN_LEFT 269
+#define RETURN_RIGHT 400
+//记录对局按钮范围
+#define RECORD_END_UP 272
+#define RECORD_END_DOWN 412
+#define RECORD_END_LEFT 439
+#define RECORD_END_RIGHT 570
+//精彩重播按钮范围
+#define REPLAY_UP 272
+#define REPLAY_DOWN 412
+#define REPLAY_LEFT 609
+#define REPLAY_RIGHT 740
 enum which_side{
     WHITE_SIDE,BLACK_SIDE//分别是白方下棋和黑方下棋
 };
@@ -40,7 +56,7 @@ int black_using_skin_num;
 int white_money;
 int black_money;
 IMAGE pic_chess_board,white_skin[2],white_skin_mask[2],black_skin[2],black_skin_mask[2];
-IMAGE white_win,white_win_mask,black_win,black_win_mask;
+IMAGE white_win,black_win;
 vector <struct point> game_record;
 MOUSEMSG m_mouse;
 
@@ -50,19 +66,17 @@ int main(){
     play();
 }
 void load_image(){
-    loadimage(&pic_chess_board,L"qipan.png",1110,774);
+    loadimage(&pic_chess_board,L"qipan.jpg",1110,774);
     loadimage(&white_skin_mask[0],L"jian.jpg",46,46);
     loadimage(&white_skin[0],L"jianjian.jpg",46,46);
     loadimage(&black_skin_mask[0],L"kun.jpg",46,46);
     loadimage(&black_skin[0],L"kunkun.jpg",46,46);
-    loadimage(&white_win_mask,L"jian.jpg",46,46);
-    loadimage(&white_win,L"jianjian.jpg",46,46);
-    loadimage(&black_win_mask,L"kun.jpg",46,46);
-    loadimage(&black_win,L"kunkun.jpg",46,46);
+    loadimage(&white_win,L"whitewin.jpg");
+    loadimage(&black_win,L"blackwin.jpg");
 }
 void game_init(){
     //棋盘置零，棋局记录清空，双方使用0号皮肤，双方金钱清零
-    initgraph(1110, 774);
+	initgraph(1110, 774);    
     memset(chess_board,0,sizeof(chess_board));
     game_record.clear();
     play_side_now=BLACK_SIDE;
@@ -74,14 +88,35 @@ void game_init(){
 }
 bool in_range_set_chess(MOUSEMSG m_mouse){
     //判断是否在下棋范围之内
-    if(m_mouse.x>=CHESS_UP && m_mouse.x<=CHESS_DOWN && m_mouse.y>=CHESS_LEFT && m_mouse.y<=CHESS_RIGHT){
+    if(m_mouse.x>=CHESS_LEFT && m_mouse.x<=CHESS_RIGHT && m_mouse.y>=CHESS_UP && m_mouse.y<=CHESS_DOWN){
+        return true;
+    }
+    else return false;
+}
+bool in_range_return(MOUSEMSG m_mouse){
+    //判断是否在下棋范围之内
+    if(m_mouse.x>=RETURN_LEFT && m_mouse.x<=RETURN_RIGHT && m_mouse.y>=RETURN_UP && m_mouse.y<=RETURN_DOWN){
+        return true;
+    }
+    else return false;
+}
+bool in_range_record_end(MOUSEMSG m_mouse){
+    //判断是否在下棋范围之内
+    if(m_mouse.x>=RECORD_END_LEFT && m_mouse.x<=RECORD_END_RIGHT && m_mouse.y>=RECORD_END_UP && m_mouse.y<=RECORD_END_DOWN){
+        return true;
+    }
+    else return false;
+}
+bool in_range_replay(MOUSEMSG m_mouse){
+    //判断是否在下棋范围之内
+    if(m_mouse.x>=REPLAY_LEFT && m_mouse.x<=REPLAY_RIGHT && m_mouse.y>=REPLAY_UP && m_mouse.y<=REPLAY_DOWN){
         return true;
     }
     else return false;
 }
 bool in_range_withdraw(MOUSEMSG m_mouse){
     //判断是否在悔棋按钮范围之内
-    if(m_mouse.x>=WITHDRAW_UP && m_mouse.x<=WITHDRAW_DOWN && m_mouse.y>=WITHDRAW_LEFT && m_mouse.y<=WITHDRAW_RIGHT){
+    if(m_mouse.x>=WITHDRAW_LEFT && m_mouse.x<=WITHDRAW_RIGHT && m_mouse.y>=WITHDRAW_UP && m_mouse.y<=WITHDRAW_DOWN){
         return true;
     }
     else return false;
@@ -160,7 +195,7 @@ int is_five(int row,int column,int which_side){
 	}
 	return k;
     }
-int one_side_win(int which_side){
+int is_one_side_win(int which_side){
     if(which_side == WHITE_SIDE){
         for(int i=0;i<15;i++){
             for(int j=0;j<15;j++){
@@ -190,19 +225,40 @@ void game_reset(){
     black_using_skin_num=0;
     draw_chess_board(chess_board);
 }
+void game_replay(){
+
+}
+void after_win_action(){
+    int flag=1;
+    while(flag){
+        m_mouse = GetMouseMsg();
+        switch (m_mouse.uMsg) {	
+            case WM_LBUTTONDOWN:{
+                if(in_range_return(m_mouse)){
+                    flag=0;
+                }
+                else if(in_range_record_end(m_mouse)){
+                    save_game_record();
+                        }
+                else if(in_range_replay()){
+                    flag=0;
+                    game_replay();
+                }
+            }
+        }
+    }
+}
 void win(int which_side){
     if(which_side== WHITE_SIDE){
         white_money+=10;
-        putimage(white_money, white_money, &white_win_mask,NOTSRCERASE);
-        putimage(white_money, white_money, &white_win,SRCINVERT);
-        //这里要加入胜利后的画面和按钮
+        putimage(0, 0, &white_win);
+        after_win_action();
 		game_reset();
     }
     else if(which_side == BLACK_SIDE){
         black_money+=10;
-        putimage(black_money, black_money, &black_win_mask,NOTSRCERASE);
-        putimage(black_money, black_money, &black_win,SRCINVERT);
-        //这里要加入胜利后的画面和按钮
+        putimage(0, 0, &black_win);
+        after_win_action();
         game_reset();
     }
 
@@ -225,10 +281,10 @@ void play(){
             case WM_LBUTTONDOWN:{
                 if(in_range_set_chess(m_mouse)){
                     set_chess(m_mouse,play_side_now);
-                    if(one_side_win(WHITE_SIDE)){
+                    if(is_one_side_win(WHITE_SIDE)){
                         win(WHITE_SIDE);
                     }
-                    else if(one_side_win(BLACK_SIDE)){
+                    else if(is_one_side_win(BLACK_SIDE)){
                         win(BLACK_SIDE);
                     }
                 }
